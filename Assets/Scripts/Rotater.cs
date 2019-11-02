d21a1bbc4a61bf8c28c6a1f1dd0275163a22d964
TheSteveIAm <TheSteveIAm@Gmail.com>
2019-11-02 12:00:51 -0500

    Init Commit
    Sperhoid movement, basic sun and moon lighting

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public float speedX = 0.1f, speedY = 1, speedZ = 0;
+    new Transform transform;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Mathf.Sin(speedX * Time.timeSinceLevelLoad), speedY, speedZ);
+        
+    }
+}
