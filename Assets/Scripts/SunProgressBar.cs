0b911ba1313e921c28622278ccd0b8d87622b025
TheSteveIAm <TheSteveIAm@Gmail.com>
2019-11-02 16:21:48 -0500

    start progress bars

---
+++ Assets/Scripts/SunProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SunProgressBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Slider progressBar;
+
+    void Awake()
+    {
+        progressBar = GetComponent<Slider>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
